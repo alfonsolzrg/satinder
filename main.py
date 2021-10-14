825f45161b9fa58e409e2bde14aa2cdd95825276
Alfonso Lizarraga <alfonso.lzrg@gmail.com>
2021-10-13 23:59:28 -0500

    Adecuaciones para funcionar en linux

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-import ctypes
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
 import os
 from pathlib import Path
 
@@ -16,10 +16,13 @@ from bs4 import BeautifulSoup
 
 from playsound import playsound
 
-ctypes.windll.kernel32.SetConsoleTitleW("SATINDER")
 options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-
+options.add_argument("--no-sandbox") # Bypass OS security model
+# options.add_experimental_option("excludeSwitches", ["enable-logging"])
+options.add_argument("--disable-dev-shm-usage") # overcome limited resource problems
+options.add_argument("--disable-infobars"); # disabling infobars
+options.add_argument("--disable-extensions"); # disabling extensions
+# options.add_argument('--headless')
 
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -35,7 +38,7 @@ def resource_path(relative):
 
 now = datetime.now()
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options,)
+driver = webdriver.Chrome(executable_path='/snap/bin/chromium.chromedriver', options=options,)
 driver.get("https://citas.sat.gob.mx/citasat/agregarcita.aspx")
 
 while True:
@@ -44,7 +47,8 @@ while True:
         cls()
         print("Llene los campos necesarios en la pagina y NO cierre el navegador ni esta ventana.")
         print("Esperando calendario...")
-        elem = WebDriverWait(driver, 240).until(expected_conditions.presence_of_element_located((By.ID, "Calendario")))
+        elem = WebDriverWait(driver, 240).until(
+            expected_conditions.presence_of_element_located((By.ID, "Calendario")))
     except Exception as e:
         print(e)
         pass
@@ -53,14 +57,18 @@ while True:
         print("Llene los campos necesarios en la pagina y NO cierre el navegador ni esta ventana.")
         print("Calendario encontrado.")
         print("Elija el mes en el que busca su cita y espere")
-        soup = BeautifulSoup(driver.page_source.encode(encoding='utf-8').strip(), 'html.parser')
+        soup = BeautifulSoup(driver.page_source.encode(
+            encoding='utf-8').strip(), 'html.parser')
         html = soup.find("table", {"id": "Calendario"})
         while True:
             sleep(60)
             driver.execute_script("location.reload()")
             print("Actualizando calendario.")
-            soup = BeautifulSoup(driver.page_source.encode(encoding='utf-8').strip(), 'html.parser')
+            soup = BeautifulSoup(driver.page_source.encode(
+                encoding='utf-8').strip(), 'html.parser')
             new_html = soup.find("table", {"id": "Calendario"})
+            if not new_html:
+                continue
 
             if html == new_html:
                 dt_string = now.strftime("%H:%M:%S")
